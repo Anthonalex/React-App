c12d0010d42f65deb8bb3c25f3e1c265c61a76e8
Anthonalex <Anthonalex@users.noreply.github.com>
2021-04-19 19:10:32 +0400

    increse and decrease and reset btns added

--- src/components/Counter/counter.jsx
+++ src/components/Counter/counter.jsx
@@ -1,20 +1,51 @@
 import React from "react";
 
 export default class Counter extends React.Component {
-  
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      counter: Number(localStorage.getItem("count")) || 0,
+    };
+  }
 
+  componentDidUpdate() {
+    localStorage.setItem("count", this.state.counter);
+  }
+
+  handleInc = () => {
+    this.setState((prevState) => ({
+      counter: prevState.counter + 1,
+    }));
+  };
+
+  handleDec = () => {
+    this.setState((prevState) => ({
+      counter: prevState.counter - 1,
+    }));
+  };
+
+  handleReset = () => {
+    this.setState({
+      counter: 0,
+    });
+
+    localStorage.clear();
+  };
   render() {
     return (
       <div>
-<h1>hrbdfgsgbvdsfvchbtyrgsvfds</h1>
-<ul>
-  <li>
-    fnsbvfc
-  </li>
-</ul>
+        <h1>Counter</h1>
+        <p>{this.state.counter}</p>
+        <button onClick={this.handleInc}>+</button>
+        <button
+          onClick={this.handleDec}
+          disabled={this.setState.counter < 0 ? true : false}
+        >
+          -
+        </button>
+        <button onClick={this.handleReset}>Reset</button>
       </div>
-      
-    )
+    );
   }
 }
