d3bcfb5ae9f21b0e923e348ccb1e2c381b729324
Anthonalex <Anthonalex@users.noreply.github.com>
2021-04-22 22:09:26 +0400

    react-select added for Searchbar task,project done

--- src/components/Search/search.jsx
+++ src/components/Search/search.jsx
@@ -1,11 +1,40 @@
 import React from "react";
+import "./search.css";
+import Select from "react-select";
+
+const options = [];
+function renderCountires() {
+  let url = "https://restcountries.eu/rest/v2/all";
+
+  fetch(url)
+    .then((data) => data.json())
+    .then((data) => {
+      data.map((el) => {
+        let push = { value: [el.name], label: [el.name] };
+        options.push(push);
+      });
+      console.log(typeof options);
+    });
+}
+
+renderCountires();
 
 export default class Search extends React.Component {
+  state = {
+    selectedOption: null,
+  };
+  handleChange = (selectedOption) => {
+    this.setState({ selectedOption });
+  };
   render() {
+    const { selectedOption } = this.state;
+
     return (
-      <div>
-        <h1>Search Bar</h1>
-      </div>
+      <Select
+        value={selectedOption}
+        onChange={this.handleChange}
+        options={options}
+      />
     );
   }
 }
